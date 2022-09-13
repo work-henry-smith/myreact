b5d95f64295c5f655cd169926703e1fc4f50c8e1
Henry Smith <work.henrysmith@gmail.com>
2022-09-13 14:19:08 +0700

    more complex for getting data from json

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import {AiFillBug} from 'react-icons/ai';
-import {BiTrash} from 'react-icons/bi';
 import AddAppointment from './components/AddAppointment';
 import Search from './components/Search';
 import List from './data.json';
+import PropList from './components/PropList'
 
 function App() {
   return (
@@ -14,21 +14,7 @@ function App() {
       <Search />
 
       <ul className='divide-y divide-gray-200'>
-        {List.map(appointment => (
-          <li className="px-3 py-3 flex items-start">
-          <button type="button"
-            className="p-1.5 mr-1.5 mt-1 rounded text-white bg-red-500 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
-            <BiTrash /></button>
-          <div className="flex-grow">
-            <div className="flex items-center">
-              <span className="flex-none font-medium text-2xl text-blue-500">{appointment.petName}</span>
-              <span className="flex-grow text-right">{appointment.aptDate}</span>
-            </div>
-            <div><b className="font-bold text-blue-500">Owner:</b>{appointment.ownerName}</div>
-            <div className="leading-tight">{appointment.aptNotes}</div>
-          </div>
-        </li>
-        ))}
+        {List.map(appointment => (<PropList key={appointment.id} appointment={appointment} />))}
       </ul>
 
     </div>
